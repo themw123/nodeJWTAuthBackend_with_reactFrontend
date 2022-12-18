86415cb7064d495ba01d56f9e79854a0982587fb
themw123 <marvin.w6@web.de>
2022-12-18 13:08:38 +0100

    n

--- NodeBackend/lib/dbLogin.js
+++
@@ -1,9 +0,0 @@
-const mysql = require("mysql");
-const connection = mysql.createConnection({
-  host: "localhost",
-  user: "est",
-  database: "login",
-  password: "***REMOVED***",
-});
-connection.connect();
-module.exports = connection;
